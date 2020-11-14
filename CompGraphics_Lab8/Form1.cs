4eb6b14e3945366b05f615903fbe56a9faceb225
User <User@WIN-AK2SGH7E72N>
2020-11-14 23:08:16 +0300

    Добавьте файлы проекта.

---
+++ CompGraphics_Lab8/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompGraphics_Lab8
+{
+    public partial class Form1 : Form
+    {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Image.FromFile("D:\\fieldSprite.jpg");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
