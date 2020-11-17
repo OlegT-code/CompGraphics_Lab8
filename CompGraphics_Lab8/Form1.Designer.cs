3f237d3290ce98218b0e599bc32bbb0d1ecf3b4b
User <User@WIN-AK2SGH7E72N>
2020-11-17 16:00:58 +0300

    Changes

--- CompGraphics_Lab8/Form1.Designer.cs
+++ CompGraphics_Lab8/Form1.Designer.cs
@@ -29,25 +29,77 @@ namespace CompGraphics_Lab8
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbBackground = new System.Windows.Forms.PictureBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackground)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbBackground
+            // 
+            this.pbBackground.Location = new System.Drawing.Point(0, 63);
+            this.pbBackground.Name = "pbBackground";
+            this.pbBackground.Size = new System.Drawing.Size(983, 614);
+            this.pbBackground.TabIndex = 0;
+            this.pbBackground.TabStop = false;
+            this.pbBackground.Paint += new System.Windows.Forms.PaintEventHandler(this.pbBackground_Paint);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(425, 700);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(150, 50);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Остановить игру";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(200, 10);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(0, 17);
+            this.Label1.TabIndex = 2;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(800, 10);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(0, 17);
+            this.Label2.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.ClientSize = new System.Drawing.Size(982, 753);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.pbBackground);
+            this.KeyPreview = true;
             this.Location = new System.Drawing.Point(1000, 600);
-            this.MaximumSize = new System.Drawing.Size(1000, 600);
-            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.MaximumSize = new System.Drawing.Size(1000, 800);
+            this.MinimumSize = new System.Drawing.Size(1000, 800);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackground)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbBackground;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
     }
 }
 
--- CompGraphics_Lab8/Form1.cs
+++ CompGraphics_Lab8/Form1.cs
@@ -5,29 +5,142 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CompGraphics_Lab8
 {
     public partial class Form1 : Form
     {
+        PictureBox tank = new PictureBox();
+        PictureBox[] GroundObjects = new PictureBox[maxCountGroundObjects];
+        const int maxCountGroundObjects = 65;
+        int[,] matrix;
+        int xCountMatrix, yCountMatrix;
+
+        int speedTank = 5;
+        int x1, y1;
+        bool isPlaying = true;
+        int lives = 5;
+        Random rand = new Random();
+
+        public Form1() {
+            InitializeComponent();         
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.BackgroundImage = Image.FromFile("D:\\fieldSprite.jpg");
-            // Hello
-            // Helllooooooooooooooooooooo
-            Console.WriteLine("Hello!");
+            pbBackground.Image = Image.FromFile("D:\\fieldSprite.jpg");
+
+            tank.Image = Image.FromFile("D:\\tankFront.png");
+            x1 = pbBackground.Width / 2 - tank.Image.Width / 2;
+            y1 = pbBackground.Height - tank.Image.Height;
+            tank.BackColor = Color.Transparent;
+            tank.Location = new Point(x1, y1);
+            tank.Size = new Size(tank.Image.Width, tank.Image.Height);
+            tank.Name = "Tank";
+            pbBackground.Controls.Add(tank);
+            tank.BringToFront();
+
+            xCountMatrix = (pbBackground.Width + 50) / 50;
+            yCountMatrix = (pbBackground.Height - tank.Height + 50) / 50;
+
+            matrix = new int[xCountMatrix, yCountMatrix];
+            int typeObject, count = 0;
+            for (int i = 0; i < maxCountGroundObjects; i++)
+            {
+                GroundObjects[count] = new PictureBox();
+                typeObject = rand.Next(0, 5);
+                if (typeObject == 0) GroundObjects[count].Image = Image.FromFile("D:\\Tree_2.png");
+                else if (typeObject == 1) GroundObjects[count].Image = Image.FromFile("D:\\Tree_3.png");
+                else if (typeObject == 2) GroundObjects[count].Image = Image.FromFile("D:\\landMine_1.png");
+                else GroundObjects[count].Image = Image.FromFile("D:\\landMine_2.png");
+
+                ret:
+                int x = rand.Next(0, xCountMatrix);
+                int y = rand.Next(0, yCountMatrix - 1);
+                if (matrix[x, y] == 1) goto ret;
+                
+                matrix[x, y] = 1;
+
+                GroundObjects[count].BackColor = Color.Transparent;
+                GroundObjects[count].Location = new Point(x * 50, y * 50);
+                GroundObjects[count].Size = new Size(GroundObjects[count].Image.Width, GroundObjects[count].Image.Height);
+                GroundObjects[count].Name = "Enemy";
+                pbBackground.Controls.Add(GroundObjects[count]);
+                GroundObjects[count].BringToFront();
+
+                count++;
+            }
         }
 
-        public Form1()
+        private void btnPause_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
-
+            if (btnPause.Text == "Остановить игру")
+            {
+                btnPause.Text = "Начать игру";
+                isPlaying = false;
+            }
+            else
+            {
+                btnPause.Text = "Остановить игру";
+                isPlaying = true;
+            }
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void pbBackground_Paint(object sender, PaintEventArgs e)
         {
+            if (tank.Location.Y <= -10)
+            {
+                ShowEndDialog("Вы дошли до финала и у вас осталось " + lives +" жизней!");
+            }
+        }
 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (isPlaying)
+            {
+                if (e.KeyCode == Keys.A)
+                {
+                    if (tank.Location.X > 0)
+                    {
+                        tank.Location = new Point(tank.Location.X - speedTank, tank.Location.Y);
+                        tank.Image = Image.FromFile("D:\\tankLeft.png");
+                    }
+                }
+                else if (e.KeyCode == Keys.D)
+                {
+                    if (tank.Location.X < this.ClientRectangle.Width - tank.Size.Width)
+                    {
+                        tank.Location = new Point(tank.Location.X + speedTank, tank.Location.Y);
+                        tank.Image = Image.FromFile("D:\\tankRight.png");
+                    }
+                }
+                else if (e.KeyCode == Keys.W)
+                {
+                    if (tank.Location.Y > -10)
+                    {
+                        tank.Location = new Point(tank.Location.X, tank.Location.Y - speedTank);
+                        tank.Image = Image.FromFile("D:\\tankFront.png");
+                    }
+                }
+                else if (e.KeyCode == Keys.S)
+                {
+                    if (tank.Location.Y < pbBackground.Height - tank.Size.Height)
+                    {
+                        tank.Location = new Point(tank.Location.X, tank.Location.Y + speedTank);
+                        tank.Image = Image.FromFile("D:\\tankBack.png");
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.Enter) btnPause_Click(sender, e);
+        }
+
+        private void ShowEndDialog(string text)
+        {
+            DialogResult result;
+            result = MessageBox.Show(text);
+            if (result == DialogResult.OK) Application.Exit();
         }
     }
 }
